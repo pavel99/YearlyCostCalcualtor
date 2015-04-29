47439181f5d19e94160dfe3748676eb846e4f568
pavel99 <mailforgithub@gmail.com>
2015-04-29 23:31:41 +0600

    Inital commit

---
+++ YearlyCostCalculator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace YearlyCostCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.classLabel = new System.Windows.Forms.Label();
+            this.classContainerComboBox = new System.Windows.Forms.ComboBox();
+            this.discountCheckBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.discountCheckBox);
+            this.groupBox1.Controls.Add(this.classContainerComboBox);
+            this.groupBox1.Controls.Add(this.classLabel);
+            this.groupBox1.Location = new System.Drawing.Point(107, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 295);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculator for Yearly Cost of a stufdent";
+            // 
+            // classLabel
+            // 
+            this.classLabel.AutoSize = true;
+            this.classLabel.Location = new System.Drawing.Point(16, 37);
+            this.classLabel.Name = "classLabel";
+            this.classLabel.Size = new System.Drawing.Size(65, 13);
+            this.classLabel.TabIndex = 0;
+            this.classLabel.Text = "Select Class";
+            // 
+            // classContainerComboBox
+            // 
+            this.classContainerComboBox.FormattingEnabled = true;
+            this.classContainerComboBox.Items.AddRange(new object[] {
+            "Class 1",
+            "Class 2",
+            "Class 3",
+            "Class 4",
+            "Class 5",
+            "Class 6",
+            "Class 7",
+            "Class 8",
+            "Class 9",
+            "Class 10",
+            "Class 11",
+            "Class 12"});
+            this.classContainerComboBox.Location = new System.Drawing.Point(91, 37);
+            this.classContainerComboBox.Name = "classContainerComboBox";
+            this.classContainerComboBox.Size = new System.Drawing.Size(121, 21);
+            this.classContainerComboBox.TabIndex = 1;
+            // 
+            // discountCheckBox
+            // 
+            this.discountCheckBox.AutoSize = true;
+            this.discountCheckBox.Location = new System.Drawing.Point(91, 87);
+            this.discountCheckBox.Name = "discountCheckBox";
+            this.discountCheckBox.Size = new System.Drawing.Size(193, 17);
+            this.discountCheckBox.TabIndex = 2;
+            this.discountCheckBox.Text = "Get 5% discount for early Admission";
+            this.discountCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate Cost";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 319);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox discountCheckBox;
+        private System.Windows.Forms.ComboBox classContainerComboBox;
+        private System.Windows.Forms.Label classLabel;
+    }
+}
+
---
+++ YearlyCostCalculator/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YearlyCostCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<int, double> admissionFee = new Dictionary<int, double> { };
+        Dictionary<int, double> monthlyFee = new Dictionary<int, double> { };
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            admissionFee.Add(1, 10000);
+            admissionFee.Add(2, 10000);
+            admissionFee.Add(3, 10000);
+            admissionFee.Add(4, 10000);
+            admissionFee.Add(5, 10000);
+            admissionFee.Add(6, 12000);
+            admissionFee.Add(7, 12000);
+            admissionFee.Add(8, 12000);
+            admissionFee.Add(9, 15000);
+            admissionFee.Add(10, 15000);
+            admissionFee.Add(11, 150000);
+            admissionFee.Add(12, 150000);
+
+            monthlyFee.Add(1, 500);
+            monthlyFee.Add(2, 500);
+            monthlyFee.Add(3, 1000);
+            monthlyFee.Add(4, 1000);
+            monthlyFee.Add(5, 1000);
+            monthlyFee.Add(6, 1500);
+            monthlyFee.Add(7, 1500);
+            monthlyFee.Add(8, 1500);
+            monthlyFee.Add(9, 2000);
+            monthlyFee.Add(10, 2000);
+            monthlyFee.Add(11, 2700);
+            monthlyFee.Add(12, 2700);
+
+            if (classContainerComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a class . ");
+            }
+            else
+            {
+
+                for (int i = 1; 1 <= 12; i++)
+                {
+                    if (classContainerComboBox.SelectedIndex == i)
+                    {
+                        double totalAmount = admissionFee[i] + (monthlyFee[i] * 12);
+                        if (discountCheckBox.Checked == false)
+                        {
+                            MessageBox.Show("The total amoun is :" + totalAmount);
+                        }
+                        else
+                        {
+                            double amountWithDiscount = totalAmount - (totalAmount * 0.05);
+                            MessageBox.Show("The total amount with discount :" + amountWithDiscount);
+
+                        }
+
+                    }
+
+                }
+
+
+
+
+            }
+        }
+    }
+}
